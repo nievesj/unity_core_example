dc14b0e8abab211ebc754f4f4fa99aa93bac58a3
Jose M. Nieves <jose.nieves@gmail.com>
2018-02-08 08:52:32 -0600

    Squashed 'Assets/Plugins/_Core/' changes from dfe28d9..fec7231
    fec7231 Working on #28
    beb6dc0 Running a test.
    f2a73e5 Disabling unity cloud build manifest for now
    f8cf163 fixing runtime bug
    e724d85 adding more debug, works locally, need to check with an actual webgl build.
    4b68bff forgot debug messages
    3fce49d Working on #38. * Added first version of Asset Bundle manifest. * Refactored services, removed most hot observables, improved namespaces, and added error checking to service creation.
    5941916 Fixing bug with EditorPreferences.
    
    git-subtree-dir: Assets/Plugins/_Core
    git-subtree-split: fec7231def509e3361c59323440147f0d1529f39

---
+++ Services/AssetService/UnityCloudBuildManifest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace Core.Services.Assets
+{
+	/// <summary>
+	/// Attempts to load the file UnityCloudBuildManifest.json. This file is added to the build
+	/// when using Unity Cloud Build services.
+	/// 
+	/// https://docs.unity3d.com/Manual/UnityCloudBuildManifest.html
+	/// </summary>
+	[System.Serializable]
+	public class UnityCloudBuildManifest
+	{
+		public string scmCommitId;
+		public string scmBranch;
+		public string buildNumber;
+		public string buildStartTime;
+		public string projectId;
+		public string bundleId;
+		public string unityVersion;
+		public string xcodeVersion;
+		public string cloudBuildTargetName;
+	}
+
+	public class UnityCloufBuildManifestLoader
+	{
+		/// <summary>
+		/// Loads UnityCloudBuildManifest into a structure the rest of the system can use, 
+		/// </summary>
+		/// <returns></returns>
+		public static IObservable<UnityCloudBuildManifest> LoadBuildManifest()
+		{
+			return Observable.Create<UnityCloudBuildManifest>(
+				(IObserver<UnityCloudBuildManifest> observer)=>
+				{
+					Action<ResourceRequest> OnResourceLoaded = resource =>
+					{
+						if (resource.asset)
+						{
+							var text = resource.asset as TextAsset;
+							var manifest = JsonUtility.FromJson<UnityCloudBuildManifest>(text.text);
+
+							observer.OnNext(manifest);
+							observer.OnCompleted();
+						}
+						else
+						{
+							observer.OnNext(null);
+							observer.OnCompleted();
+						}
+					};
+
+					//FIXME: Make this a constant in case unity decides to change the name later, of the developer implements another process
+					return Resources.LoadAsync("UnityCloudBuildManifest.json").AsAsyncOperationObservable<ResourceRequest>().Subscribe(OnResourceLoaded);
+				});
+		}
+	}
+}
